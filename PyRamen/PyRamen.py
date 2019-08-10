6322d0deabdc96502b5c10bca6c9ddc2fe4cd60a
Eric Cadena <eccadnea@gmail.com>
2019-08-10 08:43:06 -0500

    PyRamen Finished Product - Code in PyRamen.py

--- PyRamen/PyRamen.py
+++ PyRamen/PyRamen.py
@@ -1,20 +1,20 @@
-# -*- coding: UTF-8 -*-
-"""PyRamen Homework Starter."""
+# Importing cvs's for PyRamen. Calculate metrics and output a txt file.
 
-# @TODO: Import libraries
+# Import Libraries
 import csv
 from pathlib import Path
 
-# @TODO: Set file paths for menu_data.csv and sales_data.csv
+# Initalize paths
 menu_filepath = Path('Resources/menu_data.csv') 
 sales_filepath = Path('Resources/sales_data.csv')
 
-# @TODO: Initialize list objects to hold our menu and sales data
+# Initilize needed variables, dict or lists.
 menu = []
 sales = []
+report = {}
+row_count = 0
 
-# @TODO: Read in the menu data into the menu list
-
+# read menu csv
 with open(menu_filepath, 'r') as csvfile:
     # print(csvfile)
     
@@ -25,9 +25,8 @@ with open(menu_filepath, 'r') as csvfile:
     
     for row in csv_reader:
         menu.append(row)
-        
-# @TODO: Read in the sales data into the sales list
-        
+
+# read sales csv
 with open(sales_filepath, 'r') as csvfile:
     # print(csvfile)
     
@@ -39,78 +38,62 @@ with open(sales_filepath, 'r') as csvfile:
     for row in csv_reader:
         sales.append(row)
 
-print(sales[0])
-
-# @TODO: Initialize dict object to hold our key-value pairs of items and metrics
-report = {}
-
-# Initialize a row counter variable
-row_count = 0
-
-# @TODO: Loop over every row in the sales list object
+# create a metric dict for each new menu item
 for sales_row in sales:
     
-    # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
-    # @TODO: Initialize sales data variables
-    sales_row[4] = sales_item
-    sales_row[3] = quantity
+    sales_item = sales_row[4] 
+    quantity = sales_row[3]
 
-    # @TODO:
-    # If the item value not in the report, add it as a new entry with initialized metrics
-    # Naming convention allows the keys to be ordered in logical fashion, count, revenue, cost, profit
     if sales_item not in report:
         report[sales_item] = {"01-count": 0, "02-revenue": 0, "03-cogs": 0, "04-profit": 0}
     else:
         continue
 
-    # @TODO: For every row in our sales data, loop over the menu records to determine a match 
-    for menu_row in menu:
-        
-        menu_row[0] = menu_item
-        menu_row[3] = menu_price
-        menu_row[4] = menu_cost
-        
-        if menu_item == sales_item:
-            print(menu_row)
-#             report[sales_item]["01-count"] += quantity
-#             report[sales_item]["02-revenue"] += price * quantity
-#             report[sales_item]["03-cogs"] += cost * quantity
-#             report[sales_item]["04-profit"] += profit * quantity
-
-        # Item,Category,Description,Price,Cost
-        # @TODO: Initialize menu data variables
-           
-
+# extra quantites from sales to report
+for sales_row in sales:
     
+    sales_item = sales_row[4]
+    quantity = sales_row[3]
+    
+    if sales_item in report:
+        report[sales_item]["01-count"] += int(quantity)
 
-        # @TODO: Calculate profit of each item in the menu data
+# extra price and cost from menu and calculate revenue and cogs
+for sales_row in sales:
+    
+    sales_item = sales_row[4]
+    quantity = sales_row[3]
+    
+    for menu_row in menu:
+        menu_item = menu_row[0]
+        menu_price = menu_row[3]
+        menu_cost = menu_row[4]
+        
+        if sales_item == menu_item:
+            report[menu_item]["02-revenue"] += (int(menu_price) * int(quantity))
+            report[menu_item]["03-cogs"] += (int(menu_cost) * int(quantity))
+        elif sales_item != menu_item:
+            continue
 
+# calculate profit using revenue and cogs
+for item, valuedict in report.items():
+     
+    for key in valuedict:
+        
+        cogs = report[item]["03-cogs"]
+        revenue = report[item]["02-revenue"]
+        
+        if key == "04-profit":
+            report[item][key] = revenue - cogs
+        else:
+            continue 
 
-        # @TODO: If the item value in our sales data is equal to the any of the items in the menu, then begin tracking metrics for that item
+# output file with metrics
+output_path = Path("PyRamen.txt")
 
+with open(output_path, 'w') as file:
+    file.write("This is the financial report for PyRamen.\n")
+    for key in report:
+        file.write(f"{key} {report[key]} \n")
 
-            # @TODO: Print out matching menu data
-
-
-
-
-
-
-            # @TODO: Cumulatively add up the metrics for each item key
             
-
-
-
-        # @TODO: Else, the sales item does not equal any fo the item in the menu data, therefore no match
-
-
-
-    # @TODO: Increment the row counter by 1
-            row_count +=
-
-# @TODO: Print total number of records in sales data
-
-
-
-
-# @TODO: Write out report to a text file (won't appear on the command line output)
